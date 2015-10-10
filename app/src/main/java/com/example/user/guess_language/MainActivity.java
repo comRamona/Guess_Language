98d9248eb0814d483e97a573c51da0ffb2f55f7b
Ramona Comanescu <com.ramona@yahoo.com>
2015-10-10 14:35:19 +0100

    first things

--- app/src/main/java/com/example/user/guess_language/MainActivity.java
+++ app/src/main/java/com/example/user/guess_language/MainActivity.java
@@ -4,14 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloTxt = (TextView)findViewById(R.id.hellotxt);
+        helloTxt.setText(readTxt());
+    }
+
+    private String readTxt(){
+
+        InputStream inputStream = getResources().openRawResource(R.raw.hello);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return byteArrayOutputStream.toString();
     }
 
 
